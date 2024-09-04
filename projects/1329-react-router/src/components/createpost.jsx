76a3a7eb512654807fd45306e318d1c595df69d5
e158432 <mintu.kumar@mastercard.com>
2024-09-04 20:01:50 +0530

    submit-data-with-fetch-method

---
+++ projects/1329-react-router/src/components/createpost.jsx
@@ -0,0 +1,72 @@
+import { useContext, useRef } from "react";
+import  { PostList } from "../store/post-list-store";
+
+const CreatePost = () =>{
+   const {addPost}              = useContext(PostList);
+   const userIdElement        = useRef();
+   const postTitleElement     = useRef();
+   const postContentElement   = useRef();
+   const postReactionsElement = useRef();
+   const postHashTagsElement  = useRef();
+
+   const handlePostSubmit =() => {
+       event.preventDefault();
+      const userId        =  userIdElement.current.value;
+      const postTitle     =  postTitleElement.current.value;
+      const postContent   = postContentElement.current.value;
+      const postReactions = postReactionsElement.current.value;
+      const postHashTags  = postHashTagsElement.current.value.split(" ") 
+
+      fetch('https://dummyjson.com/posts/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title: postTitle,
+          body: postContent,
+          reactions: postReactions,
+          userId: userId,
+          tags: postHashTags,
+          /* other post data */
+        })
+      })
+      .then(res => res.json())
+      .then(post=>addPost(post));
+      
+      
+    
+   }
+    return (
+        <>
+        <form className="create-post" onSubmit={()=>handlePostSubmit()}>
+        <div className="mb-3">
+          <label htmlFor="userId" className="form-label">Enter your user id here</label>
+          <input type="text" ref={userIdElement} className="form-control" id="userId" placeholder="Enter user id" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">Post Title</label>
+          <input type="text" ref={postTitleElement} className="form-control" id="title" placeholder="How are you felling today.." />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="post" className="form-label">Post Content</label>
+          <textarea type="text" ref={postContentElement}  rows="4" className="form-control" id="post" placeholder="Tell us more about it" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="reactions" className="form-label">Reactions</label>
+          <input type="text" ref={postReactionsElement} className="form-control" id="reactions" placeholder="How many people reacted to this post"/>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="hashtags" className="form-label">Hashtags</label>
+          <input type="text" ref={postHashTagsElement} className="form-control" id="hashtags" placeholder="Enter your hashtags using space"/>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Post</button>
+      </form>
+        </>
+    )
+}
+
+export default CreatePost;
---
+++ projects/1329-react-router/src/store/post-list-store.jsx
@@ -0,0 +1,64 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+    postList : [],
+    addPost: ()=>{},
+    addInitialPosts : ()=>{},
+    deletePost: ()=>{}
+});
+
+const postListReducer = (currPostList, action) =>{
+    let newPostList =  currPostList;
+    if(action.type === 'DELETE_POST'){
+        newPostList = currPostList.filter((post)=>post.id !== action.payload.postId)
+    }else if(action.type === 'ADD_POST'){
+        newPostList =  [action.payload, ...currPostList]
+    }else if(action.type === 'ADD_INITIAL_POST'){
+      newPostList =  action.payload.posts
+  }
+
+   return newPostList;
+}
+
+const PostListProvider = ({children}) => {
+  const [postList, dispatchPostList] =  useReducer(postListReducer,[]);
+  
+  const addPost = (post) =>{
+     dispatchPostList({
+      type:'ADD_POST',
+      payload:post,
+     })
+      
+  }
+
+  const addInitialPosts = (posts) =>{
+    dispatchPostList({
+     type:'ADD_INITIAL_POST',
+     payload:{
+       posts
+     }
+    })
+     
+ }
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type:'DELETE_POST',
+      payload: {postId,},
+    })
+  
+  }
+
+  return(
+   <PostList.Provider value={{
+    postList:postList,
+    addPost: addPost,
+    deletePost: deletePost,
+    addInitialPosts:addInitialPosts,
+
+   }} >
+    {children}
+   </PostList.Provider>
+   );
+
+}
+export default PostListProvider;
